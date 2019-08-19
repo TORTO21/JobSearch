9408dbbc931a67c80c250fef9d5193a12a3010b0
Oliver Almalel <oliver.almalel@gmail.com>
2019-08-18 18:49:19 -0700

    LC 152

--- LeetCode/152_Maximum_Product_Array.js
+++ LeetCode/152_Maximum_Product_Array.js
@@ -17,13 +17,59 @@
  */
 
 function maxProduct (nums) {
-  let runningMax = nums[0]
-  let currMax = runningMax
+  if (!(nums.length)) return 0
+  if (nums.length === 1) return nums[0]
+  let fullProduct = productFromAll(nums)
+  if (fullProduct > 0) return fullProduct
+  let leftSlice = fullProduct / productFromLastNeg(nums)
+  let rightSlice = fullProduct / productToFirstNeg(nums)
 
-  for (let i = 1; i < nums.length; i++) {
-    currMax > 0 ? currMax += nums[i] : currMax = nums[i]
-    runningMax = Math.max(runningMax, currMax)
-  }
-
-  return runningMax
+  return Math.max(fullProduct, leftSlice, rightSlice, Math.max(nums))
 }
+
+function productToFirstNeg(nums) {
+  if (!nums) return 0
+  let result = nums[0]
+  for (let i = 1; i < nums.length; i++) {
+    if (result < 0) return result
+    result *= nums[i]
+  }
+  return result < 0 ? result : null
+}
+
+function productFromLastNeg(nums) {
+  let result = nums[0]
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < 0) { result = nums[i]; continue }
+    result *= nums[i]
+  }
+  return result < 0 ? result : null
+}
+
+function productFromAll(nums) {
+  if (!(nums.length)) return 0
+  return nums.reduce((prodTotal, num) => prodTotal * num)
+}
+
+// console.log(maxProduct([-3,1,2,3,-1,4,2,-2,2,3]))
+console.log(maxProduct([-2,3,-4]))
+console.log(maxProduct([-3,1,2,3,-1,4,2,-2,2,3]))
+
+
+function maxProductSolution(nums) {
+  if (!(nums.length)) return 0
+
+  let max = nums[0]
+  let currMax = max
+  let currMin = max
+  for (let i = 1; i < nums.length; i++) {
+    let runningMax = max
+    currMax = Math.max(nums[i], currMax * nums[i], currMin * nums[i])
+    currMin = Math.min(nums[i], currMin * nums[i], runningMax * nums[i])
+    max = Math.max(max, currMax)
+  }
+  return max
+}
+
+console.log(maxProductSolution([-2,3,-4]))
+console.log(maxProductSolution([-3,1,2,3,-1,4,2,-2,2,3]))
