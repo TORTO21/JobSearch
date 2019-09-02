8f2ebee0e2ceab5aca51be75179a0de946b0e325
Oliver Almalel <oliver.almalel@gmail.com>
2019-09-02 13:13:22 -0700

    LC 322 - 3 Solutions

--- LeetCode/322_Coin_Change.js
+++ LeetCode/322_Coin_Change.js
@@ -18,6 +18,65 @@
  * @return {number}
  */
 
-function coinChange(coins, amount) {
-     
+function coinChange(coins, amount = -1, memo = {}) {
+  if (amount in memo) return memo[amount]
+  if (amount < 0) return -1
+  if (amount === 0) return 0
+  
+  let minCt = Infinity
+  for (let coin of coins) {
+      let currCt = coinChange(coins, amount - coin, memo)
+      if (currCt >= 0 && currCt < minCt) minCt = currCt + 1
+  }
+  memo[amount] = minCt === Infinity ? -1 : minCt
+  return memo[amount]
 }
+
+console.log(coinChange([1, 2, 5], 11))
+console.log(coinChange([1, 2], 11))
+console.log(coinChange([2], 3))
+console.log(coinChange([], 3))
+console.log(coinChange([3]))
+
+
+function coinChangeMemoArray(coins, amount, count = new Array(amount)) {
+  if (count[amount - 1]) return count[amount - 1]
+  if (amount < 0) return -1
+  if (amount === 0) return 0
+
+  let minCt = Infinity
+  for (let coin of coins) {
+      let res = coinChange(coins, amount - coin, count)
+      if (res >= 0 && res < minCt) minCt = 1 + res
+  }
+  count[amount - 1] = (minCt === Infinity) ? -1 : minCt
+  return count[amount - 1]
+}
+
+console.log(coinChangeMemoArray([1, 2, 5], 11))
+console.log(coinChangeMemoArray([1, 2], 11))
+console.log(coinChangeMemoArray([2], 3))
+console.log(coinChangeMemoArray([], 3))
+// console.log(coinChange([3]))
+
+
+function coinChangeDP(coins, amount) {
+  let max = amount + 1;             
+  let dp = new Array(max).fill(max)
+  dp[0] = 0
+  for (let i = 1; i <= amount; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      if (coins[j] <= i) {
+        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+      }
+    }
+  }
+  return dp[amount] > amount ? -1 : dp[amount];
+}
+
+console.log("================")
+console.log(coinChangeDP([1, 2, 5], 11))
+console.log(coinChangeDP([1, 2], 11))
+console.log(coinChangeDP([2], 3))
+console.log(coinChangeDP([], 3))
+// console.log(coinChangeDP([3]))
