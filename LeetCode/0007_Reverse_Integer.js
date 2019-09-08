38c1e174789a6a70a3ae3c244212f80964668a87
Oliver Almalel <oliver.almalel@gmail.com>
2019-09-07 23:33:41 -0700

    LC 7

--- LeetCode/0007_Reverse_Integer.js
+++ LeetCode/0007_Reverse_Integer.js
@@ -19,13 +19,69 @@
  * @param {number} x
  * @return {number}
  */
+
+// naive approach
 function reverse(x) {
   let sign = x < 0 ? "-" : ""
   let reversed = Math.abs(x).toString().split('').reverse().join('')
-  if (Number(reversed) > Infinity) return 0
+  if (Number(reversed) > 2**31-1) return 0
   return Number(sign + reversed)
 };
 
 console.log(reverse(123))
 console.log(reverse(-123))
 console.log(reverse(120))
+
+function reverseMod(x) {
+  const isNegative = x < 0;
+  x = Math.abs(x);
+  
+  let reversed = 0
+  while (x !== 0) {
+    reversed = (reversed + x % 10) * 10
+    x = x / 10 | 0
+  }
+  reversed /= 10
+  if (reversed > 2**31) return 0
+  return isNegative ? -reversed : reversed
+}
+
+// const reverse = function(x) {
+//   const isNegative = x < 0;
+//   x = Math.abs(x);
+//   let result = 0;
+  
+//   while (x) {
+//     const digit = x % 10;
+//     x = Math.floor(x / 10);
+//     result = result * 10 + digit;
+//   }
+  
+//   // restrict result to maintain settled overflow
+//   if (result > 2**31) {
+//     return 0;
+//   }
+  
+//   return isNegative ? -result : result;
+// };
+console.log("===========")
+console.log(reverseMod(123))
+console.log(reverseMod(-123))
+console.log(reverseMod(120))
+console.log(reverseMod(-1))
+console.log(reverseMod(429496729))
+
+
+// class Solution {
+//   public int reverse(int num) {
+//      long revnum = 0;
+//       while (num != 0) {
+//         revnum = revnum * 10 + (num % 10);
+//           if(revnum >= Integer.MAX_VALUE || revnum <= Integer.MIN_VALUE){
+//               return 0;
+//           }
+//         num /= 10;
+//       }
+//       return (int) revnum;
+//   }
+// }
