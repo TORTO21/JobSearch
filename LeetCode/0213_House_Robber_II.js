eedb04a5938328bf7cb80d56bca5f85592cc78ac
Oliver Almalel <oliver.almalel@gmail.com>
2019-09-16 16:07:38 -0700

    LC 213

--- LeetCode/0213_House_Robber_II.js
+++ LeetCode/0213_House_Robber_II.js
@@ -20,6 +20,30 @@
  * @return {number}
  */
 
-function rob(nums) {
+var rob = function(nums) {
+  const N = nums.length
+  if (N === 0) return 0;
+  if (N === 1) return nums[0];
   
+  // robbing first house
+  const dp = [nums[0], Math.max(nums[0], nums[1])]
+  
+  // robbing last house
+  const dp2 = [0, nums[1]]
+  
+  for (let i = 2; i < N; i++) {
+    dp[i] = i === N - 1 ? dp[i-1] : Math.max(dp[i-1], dp[i - 2] + nums[i]);
+    dp2[i] = Math.max(dp2[i-1], dp2[i - 2] + nums[i])
+  
+  }
+
+  return Math.max(dp[N - 1], dp2[N - 1])
 };
+
+
+console.log(rob([1,2,3,1]))   // => 4
+console.log(rob([2,3,2]))     // => 3
+console.log(rob([2,3,5]))     // => 5
+console.log(rob([1,3,1,5,1])) // => 8
+console.log(rob([1,3,1,1,5])) // => 8
+console.log(rob([3,1,1,5]))   // => 6
