ed2daa881a6b6733ed7ae3758d2caf92d986c8ac
Oliver Almalel <oliver.almalel@gmail.com>
2019-09-24 12:59:49 -0700

    LC 91

--- LeetCode/0091_Decode_Ways.js
+++ LeetCode/0091_Decode_Ways.js
@@ -23,5 +23,40 @@
  */
 
 function numDecodings(s) {
-  
-};
+    let n = s.length
+    if (s == null || n == 0) return 0;
+    let dp = new Array(n + 1).fill(0)
+    dp[0] = 1;
+    dp[1] = s.charAt(0) != '0' ? 1 : 0;
+    for (let i = 2; i <= n; i++) {
+      let first = Number(s.substring(i-1, i))
+      let second = Number(s.substring(i-2, i))
+      if (first >= 1 && first <= 9) dp[i] += dp[i-1]
+      if(second >= 10 && second <= 26) dp[i] += dp[i-2]
+    }
+    return dp[n];
+}
+
+console.log(numDecodings("12"))     //=> 2
+console.log(numDecodings("226"))    //=> 3
+console.log(numDecodings("22222"))  //=> 8
+
+function numDecodings2(s) {
+  const N = s.length;
+  if (N === 0) return 0;
+  const dp = Array(N + 1).fill(0);
+  dp[0] = 1;
+  dp[1] = s[0] === '0' ? 0 : 1;
+
+  for (let i = 2; i <= N; i++) {
+    if (s[i-1] !== '0') dp[i] += dp[i-1]
+    if (s[i-2] === '1' || s[i-2] === '2' && s[i-1] <= '6') dp[i] += dp[i-2]
+  }
+
+  return dp[N];
+}
+
+console.log("==========")
+console.log(numDecodings2("12"))     //=> 2
+console.log(numDecodings2("226"))    //=> 3
+console.log(numDecodings2("22222"))  //=> 8
