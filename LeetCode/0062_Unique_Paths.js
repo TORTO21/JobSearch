916f122624d84bb1ad936b1a2904b8434a481c1f
Oliver Almalel <oliver.almalel@gmail.com>
2019-09-26 09:26:34 -0700

    LC 62

--- LeetCode/0062_Unique_Paths.js
+++ LeetCode/0062_Unique_Paths.js
@@ -30,5 +30,49 @@
  */
 
 function uniquePaths(m, n) {
-    
+  let dp = new Array(m).fill().map(_ => new Array(n).fill(1))
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    }
+  }
+  return dp[m - 1][n - 1]
 };
+
+console.log(uniquePaths(3, 2))
+console.log(uniquePaths(7, 3))
+
+function uniquePathsMath(m, n) {
+  if (m === 1 || n === 1) return 1
+  m--; n--
+  if (m < n) [m, n] = [n, m]
+  let result = 1
+  for (let i = m + 1, j = 1; i <= m + n; i++, j++) {
+    result *= i;
+    result /= j;
+  }
+  return result
+}
+
+console.log("==========")
+console.log(uniquePathsMath(3, 2))
+console.log(uniquePathsMath(7, 3))
+
+
+function uniquePathsFactCombo(m, n) {
+  var fac = i => {
+    let product = 1;
+    for (let j = 1; j <= i; j++) product *= j
+    return product;
+  }
+  
+  let upper = fac(m - 1 + n - 1);
+  let lower = fac(m - 1) * fac(n - 1);
+  
+  return upper / lower;
+};
+
+console.log("==========")
+console.log(uniquePathsFactCombo(3, 2))
+console.log(uniquePathsFactCombo(7, 3))
