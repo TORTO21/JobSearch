fb4f02bdfff665156b1a61dc8f6525ff438f5ca4
Oliver Almalel <oliver.almalel@gmail.com>
2019-10-02 00:15:19 -0700

    LC 1207 - 2 solutinos

--- LeetCode/1207_Unique_Number_of_Occurrences.js
+++ LeetCode/1207_Unique_Number_of_Occurrences.js
@@ -40,12 +40,12 @@
 function uniqueOccurrences(arr) {
   let counts = {}
   arr.forEach(num => {
-    if (counts[num]++) 1
+    if (counts[num]++) null
     else counts[num] = 1
   })
 
   counts = Object.values(counts)
-  let uniques = new Set()
+  const uniques = new Set()
 
   for (let i = 0; i < counts.length; i++) {  
     if (uniques.has(counts[i])) return false
@@ -58,3 +58,23 @@ function uniqueOccurrences(arr) {
 console.log(uniqueOccurrences([1,2,2,1,1,3]))               // => true
 console.log(uniqueOccurrences([1,2]))                       // => false
 console.log(uniqueOccurrences([-3,0,1,-3,1,1,1,-3,10,0]))   // => true
+console.log(uniqueOccurrences([]))                          // => true
+
+function uniqueOccurrences2(arr) {
+  let counts = {}
+  arr.forEach(num => {
+    if (counts[num]++) 1
+    else counts[num] = 1
+  })
+
+  counts = Object.values(counts)
+  const uniques = new Set(counts)
+
+  return counts.length === uniques.size
+};
+
+console.log("==========")
+console.log(uniqueOccurrences2([1,2,2,1,1,3]))               // => true
+console.log(uniqueOccurrences2([1,2]))                       // => false
+console.log(uniqueOccurrences2([-3,0,1,-3,1,1,1,-3,10,0]))   // => true
+console.log(uniqueOccurrences2([]))                          // => true
