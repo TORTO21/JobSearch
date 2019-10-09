ab629b9a1abeb505a9eedb3819dded081175ef66
Oliver Almalel <oliver.almalel@gmail.com>
2019-10-09 12:58:04 -0700

    LC 113

--- LeetCode/0113_Clone_Graph.js
+++ LeetCode/0113_Clone_Graph.js
@@ -35,6 +35,90 @@
  * @return {Node}
  */
 
-function cloneGraph(node) {
-    
+function Node(val,neighbors) {
+  this.val = val;
+  this.neighbors = neighbors;
 };
+
+var cloneGraph = function(node) {
+    
+  const map = new Map();
+
+//copy each value first
+  const dfs = (node) => {
+      if (map.has(node)) return
+
+      let copied = new Node(node.val, null);
+      map.set(node, copied);
+      for (let i = 0; i < node.neighbors.length; i++) {
+          dfs(node.neighbors[i]);
+      }
+  }
+  dfs(node);
+
+//use map to copy the neighbors if it exists
+  for (let [node, copy] of map) {
+
+      if (node.neighbors) {
+          copy.neighbors = [];
+          for (let i = 0; i < node.neighbors.length; i++) {
+              copy.neighbors.push(map.get(node.neighbors[i]));    
+          }
+      }
+  }
+  
+  return map.get(node);
+};
+
+// sample Graph {"$id":"1","neighbors":[{"$id":"2","neighbors":[{"$ref":"1"},{"$id":"3","neighbors":[{"$ref":"2"},{"$id":"4","neighbors":[{"$ref":"3"},{"$ref":"1"}],"val":4}],"val":3}],"val":2},{"$ref":"4"}],"val":1}
+
+let node1 = new Node(1)
+let node2 = new Node(2)
+let node3 = new Node(3)
+let node4 = new Node(4)
+node1.neighbors = [node2, node4]
+node2.neighbors = [node1, node3]
+node3.neighbors = [node2, node4]
+node4.neighbors = [node1, node3]
+
+let graph = node1
+console.log(cloneGraph(graph))
+
+
+
+
+function UndirectedGraphNode(label) {
+  this.label = label
+  this.neighbors = []
+};
+
+function cloneGraph2(graph) {
+
+  if (!graph) return graph;
+  var map = {};
+  return traverse(graph);
+
+  function traverse(node) {
+    if (!map[node.label]) {
+      var newNode = new UndirectedGraphNode(node.label);
+      map[node.label] = newNode;
+      newNode.neighbors = node.neighbors.map(traverse);
+    }
+    return map[node.label];
+  }
+}
+
+
+var aNode = new UndirectedGraphNode('0');
+var bNode = new UndirectedGraphNode('1');
+var cNode = new UndirectedGraphNode('2');
+ 
+aNode.neighbors.push(bNode, cNode);
+bNode.neighbors.push(cNode);
+cNode.neighbors.push(cNode);
+ 
+console.log("=========")
+// console.log(aNode);
+// console.log(cloneGraph2(aNode));
+
+
