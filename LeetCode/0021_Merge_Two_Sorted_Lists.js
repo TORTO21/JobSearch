3aac725506d5154a7bb6a0f42b3238f72b467b85
Oliver Almalel <oliver.almalel@gmail.com>
2019-11-12 16:03:21 -0800

    LC21 - 1 solution

--- LeetCode/0021_Merge_Two_Sorted_Lists.js
+++ LeetCode/0021_Merge_Two_Sorted_Lists.js
@@ -17,6 +17,61 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+
+ function ListNode(val) {
+     this.val = val;
+     this.next = null;
+ }
+
 var mergeTwoLists = function(l1, l2) {
-    
+	if (l1 === null && l2 === null) return null
+	if (l1 === null) return l2
+	if (l2 === null) return l1
+
+	let mergedList = new ListNode()
+	let mergedHead = mergedList
+
+	if (l1.val < l2.val) {
+		mergedList.val = l1.val
+		l1 = l1.next
+	} else {
+		mergedList.val = l2.val
+		l2 = l2.next
+	}
+
+	while (l1 && l2) {
+		if (l1.val < l2.val) {
+			mergedList.next = l1
+			l1 = l1.next
+		} else {
+			mergedList.next = l2
+			l2 = l2.next
+		}
+		mergedList = mergedList.next
+	}
+
+	l1 ? mergedList.next = l1 : mergedList.next = l2
+	return mergedHead
 };
+
+let list1 = new ListNode(1)
+list1.next = new ListNode(2)
+list1.next.next = new ListNode(4)
+
+let list2 = new ListNode(2)
+list2.next = new ListNode(3)
+list2.next.next = new ListNode(4)
+
+let list3 = mergeTwoLists(list1, list2)
+let list4 = new ListNode(10)
+
+const listVals = (node) => {
+	while (node) {
+		console.log(node.val)
+		node = node.next
+	}
+}
+
+listVals(list3)
+console.log("=================")
+listVals(mergeTwoLists(list3, list4))
