81fab0a17a82adbf8dfbf44e8c24d94079379077
Oliver Almalel <oliver.almalel@gmail.com>
2020-01-02 15:28:58 -0800

    update test logic

--- LeetCode/test.js
+++ LeetCode/test.js
@@ -1,21 +1,5 @@
-var store = [0, 1, 2, 3, 4];
-
-var stored = store.reduce(function(pV, cV, cI){
-  console.log("pv: ", pV);
-  pV.push(cV);
-  return pV; // *********  Important ******
-}, []);
-
-var stored = store.reduce((pV, cV) => {
-  console.log("pv: ", pV);
-  pV.push(cV);
-  return pV; // *********  Important ******
-}, []);
-
-let num = 5
-
-const arrayFromNum = (num) => (
-  "hi"
-)
-
-console.log(arrayFromNum(5))
+for(var i=0;i<10;i++){
+  setTimeout(() => {
+    console.log(i)})
+  }
+  console.log('a')
