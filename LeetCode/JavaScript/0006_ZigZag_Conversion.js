5fe39e7b86b2672b196b10f51dbf0424c616de6b
Oliver Almalel <oliver.almalel@gmail.com>
2020-01-06 21:58:09 -0800

    complete LC 6 4th solution

--- LeetCode/JavaScript/0006_ZigZag_Conversion.js
+++ LeetCode/JavaScript/0006_ZigZag_Conversion.js
@@ -139,3 +139,21 @@ var zigZagConvertClear = function(s, numRows) {
 };
 
 console.log(zigZagConvertClear("PAYPALISHIRING", 3)) // => "PAHNAPLSIIGYIR"
+
+// by Sporkyy on LeetCode
+const zigZagConvertShort = (s, numRows) => {
+  // 1. Make an array with the zigzag sequence
+  const zigzag = [...new Array(numRows).keys()];
+  console.log(zigzag)
+  zigzag.push(...zigzag.slice(1, -1).reverse());
+  console.log(zigzag)
+  // 2. Make an array with as many strings as we need rows
+  const rows = new Array(numRows).fill('');
+  console.log(rows);
+  // 3. Append the characters to the row strings in zigzag sequence
+  [...s].forEach((c, i) => (rows[zigzag[i % zigzag.length]] += c));
+  // 4. Join the row strings in the array together
+  return rows.join('');
+};
+
+console.log(zigZagConvertShort("PAYPALISHIRING", 5)) // => "PAHNAPLSIIGYIR"
