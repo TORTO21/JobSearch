d7c7e3d94f8fd2c6d379c64d1344ece387c7f74c
Oliver Almalel <oliver.almalel@gmail.com>
2020-01-06 19:28:06 -0800

    complete LC 6 1st solution

--- LeetCode/JavaScript/0006_ZigZag_Conversion.js
+++ LeetCode/JavaScript/0006_ZigZag_Conversion.js
@@ -24,13 +24,13 @@
 // P     I
 
 // Input: s = "PAYPALISHIRING", numRows = 5
-// Output: "PHASIYIRPLIAN"
+// Output: "PHASIYIRPLIANG"
 // Explanation:
 
 // P       H
 // A     S I
 // Y   I   R
-// P L     I
+// P L     I G
 // A       N
 
 /**
@@ -40,7 +40,44 @@
  */
 
 var zigZagConvert = function(s, numRows) {
-  
+  if (numRows === 1) return s
+  // initialize an empty result string
+  let result = ""
+  // numRows * 2 - 2 = total period
+  let period = numRows * 2 - 2
+  // 1st difference = initial period and 2nd difference = 0
+  let diff1 = period, diff2 = 0
+  // pass through the string numRows times which happens to invert the
+      // 1st difference and 2nd difference
+  for (let i = 0; i < numRows; i++) {
+    // at each difference, concat character at that index into result string
+    let currStrIdx = i
+    let useFirst = diff2 === 0 ? true : diff1 === 0 ? false : true
+    while (currStrIdx < s.length) {
+      result += s[currStrIdx]
+      if (useFirst) {
+        currStrIdx += diff1
+        useFirst = diff2 === 0 ? true : false
+      } else {
+        currStrIdx += diff2
+        useFirst = diff1 === 0 ? false : true
+      }
+    }
+    // after each pass, increment 1st difference by 2 and decrement 2nd by 2
+    diff1 -= 2
+    diff2 += 2
+  }
+      
+  // return the result string
+  return result
 };
 
-console.log(zigZagConvert("PAYPALISHIRING", 3)) // => "PINALSIGYAHRPI"
+console.log(zigZagConvert("PAYPALISHIRING", 1)) // => "PAHNAPLSIIGYIR"
+console.log(zigZagConvert("PAYPALISHIRING", 3)) // => "PAHNAPLSIIGYIR"
+console.log(zigZagConvert("PAYPALISHIRING", 4)) // => "PINALSIGYAHRPI"
+console.log(zigZagConvert("PAYPALISHIRING", 5)) // => "PHASIYIRPLIGAN"
+console.log(zigZagConvert("A", 1))              // => "A"
+console.log(zigZagConvert("AB", 2))             // => "AB"
+console.log(zigZagConvert("ABC", 2))            // => "ACB"
+console.log(zigZagConvert("ABC",  3))           // => "ABC"
+console.log(zigZagConvert("ABC",  4))           // => "ABC"
